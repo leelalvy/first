08d55ac001109761d42ad0ed181a62d933a05b19
leelalvy <leelalvy@gmail.com>
2025-03-10 11:29:49 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 class hello {
     public static void main(String[] args) {
-        System.out.println("Hello!! Welcome To SE Lab");
+        System.out.println("Hello!! Welcome To SE Lab, Jenkins");
     }
 }
